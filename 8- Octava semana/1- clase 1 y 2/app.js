42533060472062bc4be4d2d7b58b0d2256043964
Jonnad89 <jonnadvwork@gmail.com>
2025-08-07 21:34:51 -0300

    lector de manga

--- 8- Octava semana/1- clase 1 y 2/app.js
+++ 8- Octava semana/1- clase 1 y 2/app.js
@@ -6,3 +6,18 @@ boton.addEventListener("click", () => {
     body.classList.toggle("modo-claro");
     boton.textContent = body.classList.contains("modo-oscuro") ? "☀️ Modo Claro" : "🌙 Modo Oscuro"
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section-hidden");
+
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("section-visible", "section-animate");
+                obs.unobserve(entry.target); // Evita que se repita
+            }
+        });
+    }, { threshold : 0.1 });
+
+    sections.forEach(sec => observer.observe(sec))
+});
