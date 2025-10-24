1203738b9a00f4c987366f38ad0635d0ed3f0431
Jonnad89 <jonnadvwork@gmail.com>
2025-10-23 21:02:44 -0300

    JS

---
+++ Ejercicios De Logica/23/ejercicio1y2.js
@@ -0,0 +1,5 @@
+/**
+ * Separa un array de notas en dos grupos: aprobados (>= 7) y reprobados (< 7).
+ * @param {number[]} notas Array de números que representan las calificaciones.
+ * @returns {object} Un objeto con dos arrays: {aprobados, reprobados}.
+ */
