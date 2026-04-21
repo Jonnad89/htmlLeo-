1d4b647ba4234ac55202e69a31b1019a6fa1634b
Jonnad89 <jonnadvwork@gmail.com>
2026-04-21 20:57:19 -0300

    ejercicios de logica

---
+++ 2026/Abril/codigo de repaso/ejercicios de logica/ejercicios.js
@@ -0,0 +1,11 @@
+/**
+ * 1. rear una funcion que reciba una frase y devuelva un objeto indicando 
+ cuantas vececs aparece cada letra 
+ * ejemplo analizar("hola") -> (h:1, o:1, l:1, a:1)
+
+
+ 2. Crear una función que determine si una palabra o frase es un palindromo
+  (se lee igual de atrás hacia adelante), ignorando espacios y mayusculas
+
+  ejemplo: esPalindromo("Neuquen") -> true
+*/
